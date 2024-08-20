6cd185d37e7aedcec2c65edb318cc6dfe0ccd645
TheusGeto <matheusdbrwolf2021@gmail.com>
2024-08-20 16:52:41 -0300

    Add files via upload

---
+++ 2numeros.py
@@ -0,0 +1,13 @@
+import os
+os.system("cls || clear")
+
+numero1 = int(input("digite um numero: "))
+numero2 = int(input("digite outro numero: "))
+
+soma = numero1+numero2
+media= numero1-numero2
+produto = numero1*numero2
+
+print(f" aqui a media: {media}")
+print(f"aqui a soma: {soma}")
+print(f"aqui o produto: {produto}")
