5d85b6fa846b0e70db8472f82f1b1c210c8e54c7
TheusGeto <matheusdbrwolf2021@gmail.com>
2024-08-21 13:38:51 -0300

    Add files via upload

---
+++ login e senha.py
@@ -0,0 +1,17 @@
+import os
+os.system("cls || clear")
+
+login = str(input("digite seu login: "))
+senha = int(input("digite sua senha: "))
+
+if login == "Theus"and senha == 4609:
+    print("**BEM VINDO THEUS**")
+
+else:
+    ("**senha e login incorretos**")
+
+
+
+
+
+
