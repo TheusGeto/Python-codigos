7ed087c561b9c5ddf025d281415727791088875d
TheusGeto <matheusdbrwolf2021@gmail.com>
2024-08-26 16:53:10 -0300

    Add files via upload

---
+++ contagem.py
@@ -0,0 +1,9 @@
+import os
+import time
+os.system("cls || clear")
+
+soma = 0
+numero = int(input("digite um numero: "))
+for i in range(numero,0,-1):
+    print(f" contagem: {i} ")
+    time.sleep(1)
