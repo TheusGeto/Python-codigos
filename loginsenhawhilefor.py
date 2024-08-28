a62ef910df3490afaef8f9e2e1422ba21339fcf6
TheusGeto <matheusdbrwolf2021@gmail.com>
2024-08-28 16:00:36 -0300

    Add files via upload

---
+++ loginsenhawhilefor.py
@@ -0,0 +1,10 @@
+import os
+os.system("cls || clear")
+quantidade_dados = 2
+while True:
+    login =str(input(f"Digite seu login: "))
+    senha = str(input("Digite sua senha: "))
+    if login == "Theus" and senha == "Theussola2209":
+        break
+
+print("****BEM VINDO THEUS O GIGANTE****")
